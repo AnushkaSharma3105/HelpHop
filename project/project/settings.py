cfc6b0ed122c43163e94e660af7b551ba2b32e26
AnushkaSharma3105 <sharma.anushka3105@gmail.com>
2026-02-19 15:29:20 +0530

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -123,6 +123,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'forlaptopuse31@gmail.com'
-EMAIL_HOST_PASSWORD = 'drsmckvzlsvtwjmq'
+EMAIL_HOST_USER = 'enter_your_email'
+EMAIL_HOST_PASSWORD = 'enter_app_password'
 
